e9bc1930fdae2493a44742924d7a19584f12ffbf
ev15963 <ev15963@hs.ac.kr>
2020-12-12 21:45:51 +0900

    API (HttpMessageConverter) 21:45

--- HelloSpring/src/main/java/hello/HelloSpring/controller/HelloController.java
+++ HelloSpring/src/main/java/hello/HelloSpring/controller/HelloController.java
@@ -4,19 +4,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-    model.addAttribute("data", "spring!!");
-    return "hello";
+        model.addAttribute("data", "spring!!");
+        return "hello";
     }
 
     @GetMapping("hello-mvc")
     public String HelloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name, Model model) {
+        return "hello " + name; // "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name1) {
+        Hello hello = new Hello();
+        hello.setName(name1);
+        return hello;
+    }
+
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
