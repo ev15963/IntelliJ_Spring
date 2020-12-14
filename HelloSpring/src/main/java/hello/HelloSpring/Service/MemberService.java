cbf7e78d38b5cf08859983e6dff131e0c1baadcf
ev15963 <ev15963@hs.ac.kr>
2020-12-14 13:09:15 +0900

    회원 서비스 개발, 회원 서비스 테스트, 회원 서비스 코드 DI 13:08

--- HelloSpring/src/main/java/hello/HelloSpring/Service/MemberService.java
+++ HelloSpring/src/main/java/hello/HelloSpring/Service/MemberService.java
@@ -4,29 +4,44 @@ import hello.HelloSpring.domain.Member;
 import hello.HelloSpring.repository.MemberRepository;
 import hello.HelloSpring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository= memberRepository;
+    }
 
     /**
      * 회원 가입
      */
 
     public Long join(Member member) {
-
-        memberRepository.findByName(member.getName())
-        .ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
-
-        //validateDuplicateMember(member);
+        // 같은 이름의 중복회원 X
+        validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName()).ifPresent(m -> );
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
     }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findByAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
 }
