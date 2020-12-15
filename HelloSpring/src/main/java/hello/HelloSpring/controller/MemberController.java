334166e03b361e043b2f6b612e19c05a4a7cdb1a
ev15963 <ev15963@hs.ac.kr>
2020-12-15 16:56:20 +0900

    회원 관리 예제 - 웹 MVC 개발 중 회원 웹 기능 - 홈 화면 추가, 등록 16:55

--- HelloSpring/src/main/java/hello/HelloSpring/controller/MemberController.java
+++ HelloSpring/src/main/java/hello/HelloSpring/controller/MemberController.java
@@ -1,9 +1,13 @@
 package hello.HelloSpring.controller;
 
 import hello.HelloSpring.Service.MemberService;
+import hello.HelloSpring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class MemberController {
 
     private final MemberService memberService;
@@ -11,5 +15,23 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new  Member();
+        member.setName(form.getName());
+
+        System.out.println("member1 = "+member.getName());
+
+        memberService.join(member);
+        System.out.println("member2 = "+member.getName());
+
+        return "redirect:/";
     }
 }
