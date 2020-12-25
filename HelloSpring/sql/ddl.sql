1a69867514fac91d4fbb84b0e91de28d572df74f
ev15963 <ev15963@hs.ac.kr>
2020-12-25 23:14:58 +0900

    oracle 데이터베이스 연동 (sql 이용) 및 순수 JDBC 연동 중 23:14

---
+++ HelloSpring/sql/ddl.sql
@@ -0,0 +1,8 @@
+drop table member_intellij;
+create table member_intellij
+(
+    id varchar2 (255) primary key,
+    name varchar2(255)
+);
+
+insert into member_intellij (id, name) values ('spring', 'spring');
