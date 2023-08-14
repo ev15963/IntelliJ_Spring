ebe9d6397ca6aada06f55d5c584e09a5b52a7c84
ev15963 <ev15963@hs.ac.kr>
2023-08-14 21:51:44 +0900

    findbyname, findbyall memberRowMapper() 적용

--- HelloSpring/src/main/java/hello/HelloSpring/repository/JdbcTemplateMemberRepository.java
+++ HelloSpring/src/main/java/hello/HelloSpring/repository/JdbcTemplateMemberRepository.java
@@ -6,48 +6,46 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-//public class JdbcTemplateMemberRepository implements MemberRepository{
-//
-//    private final JdbcTemplate jdbcTemplate;
-//
-//    @Autowired
-//    public JdbcTemplateMemberRepository(DataSource dataSource) {
-//        jdbcTemplate = new JdbcTemplate(dataSource);
-//    }
-//
-//    @Override
-//    public Member save(Member member) {
-//        return null;
-//    }
-//
-////    @Override
-////    public Optional<Member> findById(Long id) {
-////        List<Member> result = jdbcTemplate.query("select * from member_intellij where id = ?", id);
-////        return result.stream().findAny();
-////    }
-//
-//    @Override
-//    public Optional<Member> findByName(String name) {
-//        return Optional.empty();
-//    }
-//
-//    @Override
-//    public List<Member> findByAll() {
-//        return null;
-//    }
-//
-//    private RowMapper<Member> memberRowMapper() {
-//        return (rs, rowNum) -> {
-//
-//            Member member =new Member();
-//            member.setId(rs.getLong("id"));
-//            member.setName(rs.getString("name"));
-//            return member;
-//        };
-//    }
-//}
+public class JdbcTemplateMemberRepository implements MemberRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("select * from member_intellij where id = ?", id);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findByAll() {
+        return jdbcTemplate.query("select * from member where name =?", memberRowMapper());
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+            return (rs, rowNum) -> {
+               Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return member;
+            };
+    }
+}
