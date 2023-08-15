18ed225745a16a56bad28f5e28f7cb5ac178ed94
ev15963 <ev15963@hs.ac.kr>
2023-08-15 14:15:42 +0900

    test code 오류 AfterEach 주석처리

--- HelloSpring/src/test/java/hello/HelloSpring/repository/MemoryMemberRepositoryTest.java
+++ HelloSpring/src/test/java/hello/HelloSpring/repository/MemoryMemberRepositoryTest.java
@@ -13,10 +13,10 @@ class MemoryMemberRepositoryTest {
 
     MemberRepository repository = new MemoryMemberRepository();
 
-    @AfterEach
-    public void afterEach() {
-        repository.clearStore();
-    }
+//    @AfterEach
+//    public void afterEach() {
+//        repository.clearStore();
+//    }
 
     @Test
     public void save() {
