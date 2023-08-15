f6c7f9ce99cdf07e80761a63bec258ee70b884c5
ev15963 <ev15963@hs.ac.kr>
2023-08-15 23:10:41 +0900

    spring config entity manager 추가

--- HelloSpring/src/main/java/hello/HelloSpring/SpringConfig.java
+++ HelloSpring/src/main/java/hello/HelloSpring/SpringConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
@@ -16,9 +17,12 @@ public class SpringConfig {
 
     private final DataSource dataSource;
 
+    private EntityManager em;
+
     @Autowired
-    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
