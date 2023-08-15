6bbdc58696efa32d4aa12fcb118afd7780f8163d
ev15963 <ev15963@hs.ac.kr>
2023-08-15 15:03:34 +0900

    member entity jpa로 변환

--- HelloSpring/src/main/java/hello/HelloSpring/domain/Member.java
+++ HelloSpring/src/main/java/hello/HelloSpring/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.HelloSpring.domain;
 
+import org.hibernate.annotations.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
